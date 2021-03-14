8ec918d109a9059b6a54bd126dcc26d9aa5f722c
Valentin Raevsky <valentin@compulab.co.il>
2021-03-14 16:22:45 +0200

    plat: ixm8mq: drr: Add other RAM support
    Combines:
    
    compulab: mx8mq: ddr: Add 0x01060008 support
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>
    
    plat: imx8mq: ddr: Add Kingston 4G support
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>
    
    plat: imx8mq: ddr: Add Alien 1G support
    
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- board/compulab/plat/imx8mq/ddr/ddr.c
+++ board/compulab/plat/imx8mq/ddr/ddr.c
@@ -49,10 +49,13 @@ struct lpddr4_tcm_desc {
 static const struct lpddr4_desc lpddr4_array_3op[] = {
 	{ .name = "Micron", .id = 0xFF020008, .size = 2048, .count = 1, .timing = { &dram_timing_2g_3op } },
 	{ .name = "Micron", .id = 0xFF000110, .size = 4096, .count = 1, .timing = { &dram_timing_ff000110_4g_3op } },
+	{ .name = "Kingston", .id = 0xFF000010, .size = 4096, .count = 1, .timing = { &dram_timing_ff000010_4g_3op } },
 	{ .name = "Samsung",.id = 0x01050008, .size = 1024, .count = 1, .timing = { &dram_timing_1g_3op } },
+	{ .name = "Samsung",.id = 0x01060008, .size = 1024, .count = 1, .timing = { &dram_timing_1g_3op } },
 	{ .name = "Samsung",.id = 0x01061010, .size = 2048, .count = 1, .timing = { &dram_timing_05_10_2g_3op} },
 	{ .name = "Nanya",  .id = 0x05000008, .size = 1024, .count = 1, .timing = { &dram_timing_1g_3op } },
 	{ .name = "Nanya",  .id = 0x05000010, .size = 2048, .count = 1, .timing = { &dram_timing_05_10_2g_3op} },
+	{ .name = "Alien",  .id = 0x52000008, .size = 1024, .count = 1, .timing = { &dram_timing_1g_3op } },
 };
 
 static unsigned int lpddr4_get_mr(void)
