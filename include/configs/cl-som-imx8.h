3aa6bfed530b104b622bc846d7df5199b157f893
Kevin Lannen <kevin@luci.com>
2026-01-22 11:46:32 -0700

    Remove and cleanup unused config parameters in cl-som-imx8.h

--- include/configs/cl-som-imx8.h
+++ include/configs/cl-som-imx8.h
@@ -13,90 +13,53 @@
 
 #ifdef CONFIG_SPL_BUILD
 /*#define CONFIG_ENABLE_DDR_TRAINING_DEBUG*/
-#define CONFIG_SPL_LDSCRIPT		"arch/arm/cpu/armv8/u-boot-spl.lds"
+
+/* SPL Stack and BSS */
 #define CONFIG_SPL_STACK		0x187FF0
-#define CONFIG_SPL_BSS_START_ADDR      0x00180000
-#define CONFIG_SPL_BSS_MAX_SIZE        0x2000	/* 8 KB */
-#define CONFIG_SYS_SPL_MALLOC_START    0x42200000
-#define CONFIG_SYS_SPL_MALLOC_SIZE    0x80000	/* 512 KB */
-#define CONFIG_SYS_SPL_PTE_RAM_BASE    0x41580000
+#define CONFIG_SPL_BSS_START_ADDR	0x00180000
+#define CONFIG_SPL_BSS_MAX_SIZE		0x2000		/* 8 KB */
 
-#define CONFIG_MALLOC_F_ADDR		0x182000 /* malloc f used before GD_FLG_FULL_MALLOC_INIT set */
+/* SPL Malloc */
+#define CONFIG_SYS_SPL_MALLOC_START	0x42200000
+#define CONFIG_SYS_SPL_MALLOC_SIZE	0x80000		/* 512 KB */
+#define CONFIG_MALLOC_F_ADDR		0x182000	/* malloc f used before GD_FLG_FULL_MALLOC_INIT set */
 
-#define CONFIG_SPL_ABORT_ON_RAW_IMAGE /* For RAW image gives a error info not panic */
+#define CONFIG_SPL_ABORT_ON_RAW_IMAGE
 
+/* Disable device model for SPL to reduce size */
 #undef CONFIG_DM_MMC
 #undef CONFIG_DM_PMIC
 #undef CONFIG_DM_PMIC_PFUZE100
 
+/* PMIC */
 #define CONFIG_SYS_I2C
-
 #define CONFIG_POWER_I2C
 #define CONFIG_POWER_PFUZE100
-#define CFG_POWER_PFUZE100_I2C_ADDR 0x08
-#endif
+#define CFG_POWER_PFUZE100_I2C_ADDR	0x08
+#endif /* CONFIG_SPL_BUILD */
 
-/* ENET Config */
-/* ENET1 */
-#if defined(CONFIG_CMD_NET)
-#define CONFIG_ETHPRIME                 "FEC"
-
-#define CONFIG_FEC_XCV_TYPE             RGMII
-#define CONFIG_FEC_MXC_PHYADDR          0
-#define FEC_QUIRK_ENET_MAC
-
-#define IMX_FEC_BASE			0x30BE0000
-
-#endif
-
-/* Initial environment variables */
+/* Environment */
 #define CFG_EXTRA_ENV_SETTINGS		\
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"fdt_addr_r=0x43000000\0"			\
+	"fdt_addr_r=0x43000000\0"
 
-/* Link Definitions */
-#define CFG_SYS_INIT_RAM_ADDR        0x40000000
-#define CFG_SYS_INIT_RAM_SIZE        0x80000
+/* Memory Layout */
+#define CFG_SYS_INIT_RAM_ADDR		0x40000000
+#define CFG_SYS_INIT_RAM_SIZE		0x80000
 
-#define CONFIG_MMCROOT			"/dev/mmcblk0p2"  /* USDHC1 */
-
-#define PHYS_SDRAM                      0x40000000
-
-/* This value will be recalculated using the ddr detection code */
-#define PHYS_SDRAM_SIZE			0x40000000
-
+#define PHYS_SDRAM			0x40000000
+#define PHYS_SDRAM_SIZE			0x40000000	/* 1 GB, recalculated at runtime */
 #define CFG_SYS_SDRAM_BASE		PHYS_SDRAM
 
-#define MEMTEST_DIVIDER   2
-#define MEMTEST_NUMERATOR 1
-
-#define CONFIG_BAUDRATE			115200
-
+/* UART */
 #define CFG_MXC_UART_BASE		UART3_BASE_ADDR
 
-/* Monitor Command Prompt */
-#undef CONFIG_SYS_PROMPT
-#define CONFIG_SYS_PROMPT		"u-boot=> "
-#define CONFIG_SYS_PROMPT_HUSH_PS2     "> "
-#define CONFIG_SYS_MAXARGS             64
-#define CONFIG_SYS_BARGSIZE CONFIG_SYS_CBSIZE
+/* eMMC/SD */
+#define CFG_SYS_FSL_USDHC_NUM		2
+#define CFG_SYS_FSL_ESDHC_ADDR		0
 
-#define CFG_SYS_FSL_USDHC_NUM	2
-#define CFG_SYS_FSL_ESDHC_ADDR       0
-
-/* I2C Configs */
-#define CONFIG_SYS_I2C_SPEED		  100000
-
-/* USB configs */
-#ifndef CONFIG_SPL_BUILD
-
-#endif
-
-#define CONFIG_USB_MAX_CONTROLLER_COUNT         2
-
-#define CONFIG_USBD_HS
-
-#define CPL_NET_DISC_LOGICS
+/* I2C */
+#define CONFIG_SYS_I2C_SPEED		100000
 
 #if defined(CONFIG_ANDROID_SUPPORT)
 #include "cl-som-imx8_android.h"
